39d43f8b445a69e3bd76312ae02b5695c8088355
Lasse Skindstad Ebert <lasse@lasseebert.dk>
2020-06-14 01:23:49 +0200

    Init mix project

---
+++ lib/crc_nif_error.ex
@@ -0,0 +1,18 @@
+defmodule CrcNifError do
+  @moduledoc """
+  Documentation for `CrcNifError`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> CrcNifError.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/crc_nif_error_test.exs
@@ -0,0 +1,8 @@
+defmodule CrcNifErrorTest do
+  use ExUnit.Case
+  doctest CrcNifError
+
+  test "greets the world" do
+    assert CrcNifError.hello() == :world
+  end
+end
