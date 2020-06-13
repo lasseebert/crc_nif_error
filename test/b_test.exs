9c88863b8c76a1ef63d4a511c7d2434b3db54369
Lasse Skindstad Ebert <lasse@lasseebert.dk>
2020-06-14 01:35:59 +0200

    Simpler tests

--- test/b_test.exs
+++ test/b_test.exs
@@ -2,23 +2,14 @@ defmodule BTest do
   use ExUnit.Case, async: true
 
   test "test 1" do
-    CRC.crc(
-      %{width: 16, poly: 0x3D65, init: 0x0000, refin: false, refout: false, xorout: 0xFFFF},
-      "foo"
-    )
+    CRC.crc(:crc_16_xmodem, <<1, 2, 3, 4, 5, 4, 3, 2, 1>>)
   end
 
   test "test 2" do
-    CRC.crc(
-      %{width: 16, poly: 0x3D65, init: 0x0000, refin: false, refout: false, xorout: 0xFFFF},
-      "foo"
-    )
+    CRC.crc(:crc_16_xmodem, <<1, 2, 3, 4, 5, 4, 3, 2, 1>>)
   end
 
   test "test 3" do
-    CRC.crc(
-      %{width: 16, poly: 0x3D65, init: 0x0000, refin: false, refout: false, xorout: 0xFFFF},
-      "foo"
-    )
+    CRC.crc(:crc_16_xmodem, <<1, 2, 3, 4, 5, 4, 3, 2, 1>>)
   end
 end
